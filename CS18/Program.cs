579a0b13e528fc4bfed5a4a8879656b1327a4437
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-30 17:18:26 +0300

    Добавьте файлы проекта.

---
+++ CS18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CS18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "()";
+            int maximumNestingDepth = 0;
+
+            foreach (char symbol in text)
+            {
+                if (symbol == '(')
+                    maximumNestingDepth++;
+                else
+                    maximumNestingDepth--;
+            
+                if (symbol == ')')
+                    maximumNestingDepth--;
+                else
+                    maximumNestingDepth++;
+            }
+
+            if (maximumNestingDepth == 0)
+            {
+                Console.WriteLine($"Корректное выражение. Макс глубина вложенности - {maximumNestingDepth}");
+            }
+            else
+                Console.WriteLine("Некорректное выражение.");
+        }
+    }
+}
